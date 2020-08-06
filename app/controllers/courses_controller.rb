a70e3ba1b926a01ede56b505fc44b485ee330185
pennd44 <pdiba@oxbowschool.org>
2020-08-06 16:37:14 -0400

    new,create,edit,update methods

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -11,6 +11,33 @@ class CoursesController < ApplicationController
     end
 
     def new
-        
+        @course=Course.new
     end
+
+    def create
+        new_course=Course.create(course_params)
+        redirect_to Course.last
+        #course_path(new_course)
+    end
+    
+    def edit
+        @course = Course.find(params[:id])
+    end
+
+    def update
+        @course = Course.find(params[:id])
+        @course.update(course_params)
+        redirect_to course_path(@course)
+    end
+
+    def destroy
+
+    end
+
+    private
+    def course_params
+        params.require(:course).permit(:title, :description, :credit_hr, :start_date, :month_duration)
+    end
+
+
 end

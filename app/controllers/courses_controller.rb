c3503bc10666c0a4d8d31287d07e3486c02cc109
pennd44 <pdiba@oxbowschool.org>
2020-08-06 17:01:34 -0400

    delete method

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -31,7 +31,9 @@ class CoursesController < ApplicationController
     end
 
     def destroy
-
+        @course = Course.find(params[:id])
+        @course.destroy
+        redirect_to courses_path
     end
 
     private

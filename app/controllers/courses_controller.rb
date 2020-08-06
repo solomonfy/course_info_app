a250d3f55e545373203e797f93f87f813cbc3938
solomonfy <fantahunsolomon@gmail.com>
2020-08-06 13:07:30 -0400

    index show done

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,16 @@
 class CoursesController < ApplicationController
+
+    def index
+        @courses = Course.all
+        render :index
+    end
+
+    def show
+        @course = Course.find(params[:id])
+        render :show
+    end
+
+    def new
+        
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Course.destroy_all
+
+10.times do
+    Course.create(title: Faker::Educator.course_name, description: Faker::Educator.degree, credit_hr: rand(1...4.0).round(2), start_date: Faker::Date.between(from: '2020-09-23', to: '2020-09-25'), month_duration: rand(3...6))
+end
